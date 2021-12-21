fc5cf6fbf22ef8aec3679fe5770d3ead24ae5e6c
MatteoFilippini <matteofilippini8@gmail.com>
2021-12-21 15:29:10 +0100

    created road map

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 console.log('JS OK');
 
-/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-il prezzo del biglietto è definito in base ai km (0.21 € al km)
-va applicato uno sconto del 20% per i minorenni
-va applicato uno sconto del 40% per gli over 65.
-L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+/* 
+    Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+
+    Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+    il prezzo del biglietto è definito in base ai km (0.21 € al km)
+    va applicato uno sconto del 20% per i minorenni
+    va applicato uno sconto del 40% per gli over 65.
+
+    L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 */
+
+/*
+    ROAD MAP
+    1- chiedere numero chilometri
+    2- chiedere eta passeggero
+    3- calcolare tariffa chilometri
+    4- calcolare sconto
+    5- calcolare tariffa totale
+*/
+
+
